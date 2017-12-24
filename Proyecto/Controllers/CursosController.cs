f8aff37ac0244fcc266e67693eef484565c1c921
Doncepillo <daniel.perezz@correoaiep.cl>
2017-12-24 12:20:33 -0300

    Proyecto

---
+++ Proyecto/Controllers/CursosController.cs
@@ -0,0 +1,97 @@
+﻿using Proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class CursosController : Controller
+    {
+        ProyectoEntities1 cnx;
+
+        public CursosController()
+        {
+            cnx = new ProyectoEntities1();
+
+        }
+        public ActionResult NuevoCurso()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult Listado()
+        {
+
+            cnx.Database.Connection.Open();
+
+            List<Curso> curso = cnx.Curso.ToList();
+            cnx.Database.Connection.Close();
+            return View(curso);
+        }
+
+        
+
+        public ActionResult Guardar(string Nombre, string Docente, int Costo, string Jornada, string Modalidad, int Cupo)
+        {
+            Curso curso = new Curso()
+            {
+               
+                Nombre = Nombre,
+                Docente = Docente,
+                Costo = Costo,
+                Jornada = Jornada,
+                Modalidad = Modalidad,
+                Cupo = Cupo
+
+            };
+
+            cnx.Curso.Add(curso);
+            cnx.SaveChanges();
+            return View("Listado", cnx.Curso.ToList());
+
+        }
+
+
+
+        public ActionResult Ficha(int Id)
+        {
+
+            return View(cnx.Curso.Where(x => x.Id == Id).First());
+
+        }
+
+
+
+
+        public ActionResult Ver(int Id)
+        {
+
+
+            return View("Ficha", null);
+        }
+
+
+        public ActionResult Eliminar(int Id)
+        {
+            cnx.Curso.Remove(cnx.Curso.Where(x => x.Id == Id).First());
+            cnx.SaveChanges();
+            return View("Listado", cnx.Curso.ToList());
+
+        }
+
+        public ActionResult Editar(int Id)
+        {
+
+            return View(cnx.Curso.Where(x => x.Id == Id).First());
+
+
+
+        }
+
+
+    }
+}
---
+++ Proyecto/Controllers/DocentesController.cs
@@ -0,0 +1,96 @@
+﻿using Proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class DocentesController : Controller
+    {
+        ProyectoEntities1 cnx;
+
+        public DocentesController()
+        {
+            cnx = new ProyectoEntities1();
+
+        }
+        public ActionResult NuevoDocente()
+        {
+
+            return View();
+        }
+
+        public ActionResult Listado()
+        {
+
+            cnx.Database.Connection.Open();
+
+            List<Docente> docente = cnx.Docente.ToList();
+            cnx.Database.Connection.Close();
+            return View(docente);
+        }
+
+
+
+        public ActionResult Guardar(string Rut, string Nombre, string Apellidos, string Sexo, string Titulo, int Telefono, string Email)
+        {
+            Docente docente = new Docente()
+            {
+                Rut = Rut,
+                Nombre = Nombre,
+                Apellidos = Apellidos,
+                Sexo = Sexo,
+                Titulo = Titulo,
+                Telefono = Telefono,
+                Email = Email
+
+            };
+
+            cnx.Docente.Add(docente);
+            cnx.SaveChanges();
+            return View("Listado", cnx.Docente.ToList());
+
+        }
+
+
+
+        public ActionResult Ficha(string Rut)
+        {
+
+            return View(cnx.Docente.Where(x => x.Rut == Rut).First());
+
+        }
+
+
+
+
+        public ActionResult Ver(string Rut)
+        {
+
+
+            return View("Ficha", null);
+        }
+
+
+        public ActionResult Eliminar(string Rut)
+        {
+            cnx.Docente.Remove(cnx.Docente.Where(x => x.Rut == Rut).First());
+            cnx.SaveChanges();
+            return View("Listado", cnx.Docente.ToList());
+
+        }
+
+        public ActionResult Editar(string Rut)
+        {
+
+            return View(cnx.Docente.Where(x => x.Rut == Rut).First());
+
+
+
+        }
+
+
+    }
+}
